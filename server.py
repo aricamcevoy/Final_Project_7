caf2df24f226fb9644a3db68f9da1b6f086a846f
Arica McEvoy <arica.mcevoy@gmail.com>
2025-02-01 23:22:08 -0500

    End of Project 7

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+Module to detect and score emotions from input
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +8,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Calculate Emotion Score
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -18,11 +24,14 @@ def sent_detector():
 
     if anger is None:
         return "Invalid Text! Please try again!"
-    else:
-       return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is {dominant}."
+
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is {dominant}."
 
 @app.route("/")
 def render_index_page():
+    """
+    Render Template
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
